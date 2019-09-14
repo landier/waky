d4fde92364ad94c1fc86f2fb30b9ab8b82c70296
Nicolas Landier <nicolas.landier@gmail.com>
2019-09-14 22:22:24 +0200

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="waky",
-    version="0.0.1",
+    version="0.0.2",
     author="Nicolas Landier",
     author_email="nicolas.landier@gmail.com",
     description="Waky is a web application to manage Wake-On-Lan supporting devices",
