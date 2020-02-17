08bb5912f6ad7a015478fe6c4ac613041a8f9e88
Nicolas Landier <nicolas.landier@gmail.com>
2020-02-17 20:58:02 +0100

    Docker + ping3

--- waky/main.py
+++ waky/main.py
@@ -1,6 +1,10 @@
 import os
 from sanic import Sanic
 from sanic.response import json, text
+from sanic.response import stream
+from ping3 import ping
+import asyncio
+
 
 app = Sanic("waky")
 
@@ -15,12 +19,22 @@ async def hosts(request):
     return text(app.config.HOSTS)
 
 
+@app.route("/ping/<host>")
+async def ping_handler(request, host):
+    async def ping_streaming_fn(response):
+        while True:
+            await response.write(str(round(ping(host, unit='ms'), 3))+"\n")
+            await asyncio.sleep(1)
+
+    return stream(ping_streaming_fn, content_type='text', chunked=True)
+
+
 def main():
     if "WAKY_CONF" in os.environ:
         app.config.from_envvar("WAKY_CONF")
     debug = app.config.get("DEBUG", False)
     listening = app.config.get("LISTENING", "0.0.0.0")
-    port = app.config.get("PORT", 8000)
+    port = app.config.get("PORT", 8888)
     app.run(host=listening, port=port, debug=debug)
 
 
