196d6bd000cb5ad2d8568d565b88843fcdd4bd49
Nicolas Landier <nicolas.landier@gmail.com>
2020-11-11 00:19:30 +0100

    Add streaming response

--- waky/main.py
+++ waky/main.py
@@ -1,6 +1,9 @@
+import asyncio
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+
 
 app = FastAPI()
 
@@ -13,3 +16,16 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+async def ping_streaming_fn():
+    while True:
+        yield "bim\n"
+        await asyncio.sleep(0.01)
+
+
+@app.get("/ping/{host}")
+async def ping_handler(host):
+    return StreamingResponse(
+        ping_streaming_fn(), media_type="text/plain; charset=utf-8"
+    )
